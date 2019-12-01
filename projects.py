00fc8e13ce7437c6a5043801634e5b0f005b68ef
Sreeram <sreeram.networking@gmail.com>
2019-11-30 20:26:26 -0500

    Added new project to list of projects

--- projects.py
+++ projects.py
@@ -3,3 +3,5 @@
 # 1. Send an email when a price drops for a product from a shopping website, example Amazon
 # 2. Sort the latest movie reviews from IMDB and download it as CSV file
 # 3. Build a Twitter chatbot to get list of followers
+
+# (Last project) Write mock tests using Mock and UnitTest 
