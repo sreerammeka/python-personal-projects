cc5fc767d178dd951c3d29d5cc746e5ac7ebdb04
Sreeram <sreeram.networking@gmail.com>
2019-11-30 20:46:05 -0500

    Added job search project

--- projects.py
+++ projects.py
@@ -3,6 +3,7 @@
 # 1. Send an email when a price drops for a product from a shopping website, example Amazon
 # 2. Sort the latest movie reviews from IMDB and download it as CSV file
 # 3. Build a Twitter chatbot to get list of followers
-4. Send alerts to email or text when a stock price falls or goes above a certain price 
+# 4. Send alerts to email or text when a stock price falls or goes above a certain price 
+# 5. Get the list of jobs with a specific job title from the list of multiple job websites like Indeed, CareerBuilder etc
 
 # (Last project) Write mock tests using Mock and UnitTest 
