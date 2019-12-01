51c347c010207b9316ea7aec98d1fdf7dbffddc3
Sreeram <sreeram.networking@gmail.com>
2019-11-30 21:18:39 -0500

    Added new projects to list of projects

--- projects.py
+++ projects.py
@@ -1,9 +1,17 @@
 #List of ongoing projects
+# Note: For alerts like SMS we can use IFTTT
 
 # 1. Send an email when a price drops for a product from a shopping website, example Amazon
 # 2. Sort the latest movie reviews from IMDB and download it as CSV file
 # 3. Build a Twitter chatbot to get list of followers
 # 4. Send alerts to email or text when a stock price falls or goes above a certain price 
 # 5. Get the list of jobs with a specific job title from the list of multiple job websites like Indeed, CareerBuilder etc
+# 6. Build Lambda functions to do some tasks on AWS, like automatically backup RDS databases and snapshots, read files on EC2 server etc
+# 7. Birthday reminder app
+# 8. Weather App
+# 9. Detect a face using OpenCV (Face recognition)
+# 10. Realtime Taxi App with Django and Angular/React
+# 11. Create a Python GUI interface using Tkinter
+
 
 # (Last project) Write mock tests using Mock and UnitTest 
