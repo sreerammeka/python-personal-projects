3f1fbdb97c0a2c3da877c1b0820a93fa75d7dca8
Sreeram <sreeram.networking@gmail.com>
2019-11-30 20:32:17 -0500

    Added stock price project

--- projects.py
+++ projects.py
@@ -3,5 +3,6 @@
 # 1. Send an email when a price drops for a product from a shopping website, example Amazon
 # 2. Sort the latest movie reviews from IMDB and download it as CSV file
 # 3. Build a Twitter chatbot to get list of followers
+4. Send alerts to email or text when a stock price falls or goes above a certain price 
 
 # (Last project) Write mock tests using Mock and UnitTest 
