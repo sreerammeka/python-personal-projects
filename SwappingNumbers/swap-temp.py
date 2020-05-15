08299735e0389d623f5d14cee7ab63766c22040c
Sreeram <sreeram.networking@gmail.com>
2020-05-14 19:30:47 -0500

    "Removed predefined input and added user input"

--- SwappingNumbers/swap-temp.py
+++ SwappingNumbers/swap-temp.py
@@ -1,9 +1,17 @@
 #Swapping numbers using a third temp variable
-a = 2
-b = 3
+# a = 2
+# b = 3
+
+#Taking values via user input
+#input() function will output a string
+a = int(input("Enter the value of a"))
+b = int(input("Enter the value of b"))
+
+#Printing to check the return data type, the return should be an integer not a string.
+print("The return types of a and b are", type(a), type(b))
 
 temp = a
 a = b
 b = temp 
 
-print("The values of a and b using a third temp variable", a,b)
+print("The values of a and b after swapping using a third temp variable are", a,b)
