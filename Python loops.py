689bf40044cb8f55d41661b8fc08724f1b7dba93
Sreeram <sreeram.networking@gmail.com>
2020-05-17 22:44:27 -0500

    "Added Nested for loops and break, continue, pass"

--- Python loops.py
+++ Python loops.py
@@ -17,17 +17,46 @@ else:
 # While can be used for printing on multiple lines
 # While executes or performs the operation as long as it fails
 # For while loop, 3 things are necessary: initialization, condition, increment or decrement
-
+# Nested while loops
 while x < 5:
-    # Specifying end will ensure that it will be on same line
+    # Specifying "end" will ensure that it will be on same line
     print("Hello, testing a while loop ", end="")
     y = 0
     while y < 3:
         print("Nested while loop ", end="")
         y+= 1
+    # incrementing the value of x, otherwise x will remain the same and will be stuck in a loop
     x+= 1
     # prints on a new line
     print()
 
 
 #for loop
+# for loop can be used to index elements in equences, i.e list, tuple, set, string etc one at a time
+
+a = ["Hello", 2.5, 3]
+b= "World"
+
+# The first for loop will index "Hello" first, then goes to second for loop and index each element separately "W", "o", "r", "l", "d",
+# then index the value 2.5 from first for loop, then index "W", "o", "r", "l", "d" from the second loop
+# then index the value  from first for loop, then index "W", "o", "r", "l", "d" from the second loop
+for i in a:
+    print(i)
+    # print(i, end=" ")
+
+    # This for loop will index each element in string one by one
+    for j in b:
+        print(j)
+        # for p in range(2,10,2):
+        # start from 11 and end before 20 with increment 2 in increasing order
+        for p in range(11, 20, 2):
+            print(p)
+
+            # print the values in decreasing order starting from 10 and ending with 1
+            for q in range(10,0,-1):
+                print(q)
+
+# break, continue, pass
+# break statement will be used to break out of a loop and halt remaining execution
+# continue statement will be used to skip execution but not break out of the loop
+# pass statement will pass the execution even though nothing is performed. It will skip the block
