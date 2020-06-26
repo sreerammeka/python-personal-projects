ac505e262353b1033179bfa2aa5d5d011bac26c9
Sreeram <sreeram.networking@gmail.com>
2020-06-25 19:53:00 -0500

    "Added code to change or set recursion limit"

--- Factorial/Factorial using recursion.py
+++ Factorial/Factorial using recursion.py
@@ -1,9 +1,20 @@
 # Program to find the factorial of a given number with recursion
 
+# To check or set recursion limit beyond 1000, we need to import sys. Default is 1000
+import sys
+
+# Change or set the recursion limit
+sys.setrecursionlimit(2000)
+# Printing the current recursion limit
+print("The current recursion limit is: ", sys.getrecursionlimit())
+
+
 result = 1
 
 
 def factorial_with_recursion(n):
+    # A global variable can be accessed and can't be modified inside a function, so that's why we have to declare
+    # using global keyword to modify that variable
     global result
 
     if n == 0:
@@ -17,3 +28,5 @@ def factorial_with_recursion(n):
 final_result = factorial_with_recursion(int(input("Enter the factorial to calculate: ")))
 
 print(final_result)
+
+
